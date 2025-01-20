cf7ef85faf34f8745397bb0c91428f4f98f7bc60
MOHDSUHEL_01 <mohdsuhel.dev@gmail.com>
2025-01-20 16:37:08 +0530

    statics page design change

--- Frontend/src/Components/Statics.jsx
+++ Frontend/src/Components/Statics.jsx
@@ -1,29 +1,32 @@
 // StatsComponent.jsx
 import React from "react";
 import { FaCog, FaProjectDiagram, FaUsers, FaSmile, FaCar } from "react-icons/fa";
+import { AiOutlineSafety } from "react-icons/ai";
 
 const stats = [
-    { id: 4, icon: <FaSmile className="text-6xl text-secondary" />, title: "Happy Clients", value: "1000+" },
-  { id: 1, icon: <FaCog className="text-6xl text-secondary" />, title: "Years Of Experience", value: "16+" },
-  { id: 2, icon: <FaCar className="text-6xl text-secondary" />, title: "Sanitized Cars for Your Safety", value: "12+" },
-  { id: 3, icon: <FaUsers className="text-6xl text-secondary" />, title: "Team Members", value: "10+" },
+    { id: 4, icon: <img src='statics/experience-4.svg'className="h-full w-full"/>, title: "Happy Clients", value: "100000+" },
+  { id: 1, icon: <img src='statics/experience-1.svg'className="h-full w-full"/>, title: "Years Of Experience", value: "16+" },
+  { id: 2, icon: <img src='statics/driver.png'className="h-full w-full"/>, title: "Professional Drivers", value: "100%" },
+  { id: 3, icon: <AiOutlineSafety className="h-full w-full"/>, title: "Safety", value: "100%" },
 ];
 
 const StatsComponent = () => {
   return (
-    <div className="relative  text-gray-200 md:py-8 py-6"  style={{
+    <div className="relative  text-gray-200 md:py-12 py-8 " data-aos='fade-up' style={{
         backgroundImage: "url('bmw-banner.webp')", 
+        backgroundPosition:'center'
       }}>
         <div className="absolute inset-0 bg-primary bg-opacity-80"></div>
       <div className="relative z-10 container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 md:px-10 px-2">
         {stats.map((stat) => (
           <div
             key={stat.id}
-            className="flex flex-col items-center text-center space-y-2"
+            className="flex flex-col items-center text-center space-y-1"
           >
             
-            <div className="icon p-4 rounded-full">{stat.icon}</div>
-            <h3 className="text-base font-semibold">{stat.title}</h3>
+            <div className="h-16 w-16 ">{stat.icon}</div>
+            <div className="bg-white h-[1px] w-24"></div>
+            <h5 className="text-base font-medium">{stat.title}</h5>
             <p className="text-3xl font-bold">{stat.value}</p>
           </div>
         ))}
